56780e212ccfe84c75037421a5eaa52c436716d8
Dude <kisang6710@gmail.com>
2022-08-07 22:15:36 +0900

    메인 홈

--- src/components/home/main.js
+++ src/components/home/main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button, Container } from '@mui/material';
 
 
 const Home = () => {
     return (
-        <div>메인</div>
+        <Container sx={ { bgcolor:"primary.main", display:"flex", flexDirection:"column" } } maxWidth="md">
+            메인
+        </Container>
     );
 };
 
