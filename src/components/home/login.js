20e59a01645bd0e50aa970871009c4ee6ad049e7
dude <masterkorea01@naver.com>
2022-08-08 16:16:17 +0900

    Design

--- src/components/home/login.js
+++ src/components/home/login.js
@@ -49,63 +49,68 @@ const Login = () => {
                 email: data.get('email'),
                 password: data.get('password'),
             };
-            SignupApi(joinData, navigate, setRegisterError);
+            console.log(joinData);
+            //SignupApi(joinData, navigate, setRegisterError);
         }
     };
     
     return (
         <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box
-            sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            }}
-        >
-            <Typography component="h1" variant="h5">
-            Demu 로그인
-            </Typography>
-            <Box component="form" onSubmit={ handleSubmit } noValidate sx={{ mt: 3 }}>
-            <FormControl component="fieldset" variant="standard">
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            autoFocus
-                            fullWidth
-                            type="email"
-                            id="email"
-                            name="email"
-                            label="이메일 주소"
-                            error={ emailError !== '' || false }
-                        />
+            <Box
+                sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                Demu 로그인
+                </Typography>
+                <Box component="form" onSubmit={ handleSubmit } noValidate sx={{ mt: 3 }}>
+                <FormControl component="fieldset" variant="standard">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                autoFocus
+                                fullWidth
+                                type="email"
+                                id="email"
+                                name="email"
+                                label="닉네임 또는 이메일"
+                                error={ emailError !== '' || false }
+                            />
+                        </Grid>
+                        <FormHelperTexts>{emailError}</FormHelperTexts>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                type="password"
+                                id="password"
+                                name="password"
+                                label="비밀번호 (숫자+영문자+특수문자 8자리 이상)"
+                                error={passwordState !== '' || false}
+                            />
+                        </Grid>
+                        <FormHelperTexts>{passwordState}</FormHelperTexts>
                     </Grid>
-                    <FormHelperTexts>{emailError}</FormHelperTexts>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            fullWidth
-                            type="password"
-                            id="password"
-                            name="password"
-                            label="비밀번호 (숫자+영문자+특수문자 8자리 이상)"
-                            error={passwordState !== '' || false}
-                        />
-                    </Grid>
-                    <FormHelperTexts>{passwordState}</FormHelperTexts>
-                </Grid>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={ { mt: 3, mb: 2, bgcolor: "warning.dark", ":hover": { bgcolor: "warning.main" } } }
-                    size="large"
-                    >
-                    로그인
-                </Button>
-                <Grid display={ "flex" }>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={ { mt: 3, mb: 2, bgcolor: "warning.dark", ":hover": { bgcolor: "warning.main" } } }
+                        size="large"
+                        >
+                        로그인
+                    </Button>
+                </FormControl>
+                <FormHelperTexts>{registerError}</FormHelperTexts>
+                </Box>
+            </Box>
+            <Grid display={ "flex" }>
                     <Button
                         type="submit"
                         fullWidth
@@ -125,10 +130,6 @@ const Login = () => {
                         구글 로그인 
                     </Button>
                 </Grid>
-            </FormControl>
-            <FormHelperTexts>{registerError}</FormHelperTexts>
-            </Box>
-        </Box>
         </Container>
     );
 };
